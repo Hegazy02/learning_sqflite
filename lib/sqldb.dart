a407737603bbe1f9464c5d9d402a2e1fc0e41c74
Hegazy02 <Abdelrhmanhegazy02@gmail.com>
2023-12-03 18:21:40 +0200

    creating sqldb file

---
+++ lib/sqldb.dart
@@ -0,0 +1,59 @@
+import 'dart:async';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class SqlDb {
+  static Database? _db;
+  Future<Database?> get db async {
+    if (_db == null) {
+      _db = await initialDb();
+      return _db;
+    } else {
+      return _db;
+    }
+  }
+
+  Future<Database> initialDb() async {
+    String databasePath = await getDatabasesPath();
+    String path = join(databasePath, 'hegazy.db');
+    Database mydb =
+        await openDatabase(path, onCreate: _onCreate, onUpgrade: _onUpgrade);
+    return mydb;
+  }
+
+  FutureOr<void> _onCreate(Database db, int version) async {
+    await db.execute('''
+    CREATE TABLE "note" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "note" TEXT NOT NULL
+  )
+  ''');
+    print("Create Database and Table ===================");
+  }
+
+  FutureOr<void> _onUpgrade(
+      Database database, int oldVersion, int newVersion) async {}
+  getData(String sql) async {
+    Database? mydb = await db;
+    List<Map> response = await mydb!.rawQuery(sql);
+    return response;
+  }
+
+  insertData(String sql) async {
+    Database? mydb = await db;
+    int response = await mydb!.rawInsert(sql);
+    return response;
+  }
+
+  updatetData(String sql) async {
+    Database? mydb = await db;
+    int response = await mydb!.rawUpdate(sql);
+    return response;
+  }
+
+  deleteData(String sql) async {
+    Database? mydb = await db;
+    int response = await mydb!.rawDelete(sql);
+    return response;
+  }
+}
